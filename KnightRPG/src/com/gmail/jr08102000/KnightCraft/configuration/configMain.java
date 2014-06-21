54e16f12947c2f5bb3cc0e75f4c892af9ff8c7c5
teyjiarong1 <jr08102000@gmail.com>
2014-06-21 22:56:54 +0800

    修改一些錯誤@@

---
+++ KnightRPG/src/com/gmail/jr08102000/KnightCraft/configuration/configMain.java
@@ -0,0 +1,26 @@
+package com.gmail.jr08102000.KnightCraft.configuration;
+
+public class configMain extends configLoader
+{
+  private static configMain instance;
+
+  private configMain()
+  {
+    super("主設定檔案.yml");
+    loadKeys();
+  }
+
+  public static configMain getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new configMain();
+    }
+
+    return instance;
+  }
+
+  protected void loadKeys()
+  {
+  }
+}
